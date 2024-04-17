7063ecc160ed13094df9b91d14c022269aeeb903
nut <suppanat.nut@gmail.com>
2024-04-18 03:33:42 +0700

    fix : js-callback

--- ex-1.js
+++ ex-1.js
@@ -1,12 +1,16 @@
 // Exercise #1: For Each Function
-
-function forEach(array, operation) {
-  // Start coding here
-}
-
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 const newEmployeeSalaries = [];
 
-// Using `forEach` function here
+function forEach(array, operation) {
+  for(let i = 0; i < array.length; i++){
+    newEmployeeSalaries.push(operation(array[i]));
+  }
+  return newEmployeeSalaries ;
+}
+
+const newBonus  = (salary) => {return salary + 5000;}
+
+forEach(employeeSalaries,newBonus);
 
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
